31d515c8e228c921ccf8cf52133448c45e7ead9a
nlea <nele.uhlemann@gmail.com>
2026-04-10 12:00:37 +0200

    add robot.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://manuelmennig.de',
   adapter: cloudflare({
     imageService: 'passthrough',
   }),
+
+  integrations: [sitemap()],
 });
